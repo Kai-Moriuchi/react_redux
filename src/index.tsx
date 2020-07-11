927e47b38c2240a46d2685930ce8b34a7815cde1
Kai <morinaikai@morinaikainoMacBook-Pro.local>
2020-07-11 21:31:07 +0900

    Reactチュートリアル終了

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,12 @@
 import React, { Component } from 'react';
 import * as ReactDOM from 'react-dom';
 import './index.css';
-
-type SquareType = string | null;
+import { setPriority } from 'os';
 
 //Squareのプロパティ
 interface SquareProps {
-    value: SquareType;
+    value: any;
     onClick: () => void;
-}
-
-interface SquareState {
-    value: string | null;
 }
 
 /*class Square extends React.Component<SquareProps, SquareState> {
@@ -28,63 +23,50 @@ interface SquareState {
 }*/
 
 //Squareを関数コンポーネント化
-function Square(props: any) {
+function Square(props: SquareProps) {
     return (
-      <button className="square" onClick={props.onClick}>
-        {props.value}
-      </button>
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
     );
-  }
+}
 
-interface BoardState {
-    squares:SquareType[];
-    xIsNext:boolean; 
+interface BoardProps {
+    squares: ('O' | 'X' | null)[];
+    onClick: (i: number) => void;
 }
 
 //BoardはReactコンポーネントクラス．propsを受け取り，renderを通して表示するビューの階層構造を返す．
-class Board extends React.Component<any, BoardState> {
-    constructor(props: any) {
+class Board extends React.Component<BoardProps, {}> {
+    /*constructor(props: any) {
         super(props);
         this.state = {
             squares: Array(9).fill(null),
             xIsNext: true,
         };
-    }
+    }*/
 
-    handleClick(i: any) {
-        const squares = this.state.squares.slice();//sliceを呼ぶことで配列のコピーを作成．
-        if (calculateWinner(squares) || squares[i]){
-            return;
-        }
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
-        this.setState({
-            squares: squares,
-            xIsNext: !this.state.xIsNext,
-        });
-    }
-
-    renderSquare(i: any) {
+    renderSquare(i: number) {
         return (
-        <Square 
-        value={this.state.squares[i]} 
-        onClick={()=> this.handleClick(i)}
-        />
+            <Square
+                value={this.props.squares[i]}
+                onClick={() => this.props.onClick(i)}
+            />
         );
     }
 
     //renderが返すのは画面上に表示したい説明書きのこと．
     render() {
-        const winner = calculateWinner(this.state.squares);
+        /*const winner = calculateWinner(this.state.squares);
         let status;
         if(winner) {
             status = 'Winner: ' + winner;
         } else {
             status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-        }
+        }*/
 
         return (
             <div>
-                <div className="status">{status}</div>
                 <div className="board-row">
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
@@ -105,16 +87,84 @@ class Board extends React.Component<any, BoardState> {
     }
 }
 
-class Game extends React.Component {
+interface HistoryData {
+    squares: ('O' | 'X' | null)[];
+}
+
+interface GameState {
+    history: HistoryData[];
+    xIsNext: boolean;
+    stepNumber: number;
+}
+
+class Game extends React.Component<{}, GameState> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            history: [{
+                squares: Array(9).fill(null),
+            }],
+            xIsNext: true,
+            stepNumber: 0,
+        };
+    }
+
+    handleClick(i: number) {
+        const history = this.state.history;
+        const current = history[history.length - 1]
+        let squares = current.squares.slice();//sliceを呼ぶことで配列のコピーを作成．
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            history: history.concat([{
+                squares,
+            }]),
+            stepNumber: history.length,
+            xIsNext: !this.state.xIsNext,
+        });
+    }
+
+    jumpTo(step: number) {
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) === 0,
+        });
+    }
+
     render() {
+        const history = this.state.history;
+        const current = history[this.state.stepNumber];
+        const winner = calculateWinner(current.squares);
+        const moves = history.map((step, move) => {
+            const desc = move ?
+                'Go to move #' + move :
+                'Go to game start';
+            return (
+                <li key={move} >
+                    <button onClick={() => this.jumpTo(move)}> {desc}</button>
+                </li>
+            )
+        });
+
+        let status;
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
         return (
             <div className="game">
                 <div className="game-board">
-                    <Board />
+                    <Board
+                        squares={current.squares}
+                        onClick={(i) => this.handleClick(i)}
+                    />
                 </div>
                 <div className="game-info">
-                    <div>{/* status */}</div>
-                    <ol>{/* TODO */}</ol>
+                    <div>{status}</div>
+                    <ol>{moves}</ol>
                 </div>
             </div>
         );
@@ -126,22 +176,26 @@ ReactDOM.render(
     document.getElementById('root')
 );
 
-function calculateWinner(squares : any) {
+//勝者判定の関数
+function calculateWinner(squares: any) {
     const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
+        //横
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        //縦
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        //斜め
+        [0, 4, 8],
+        [2, 4, 6],
     ];
     for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
-      }
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
     }
     return null;
-  }
+}
